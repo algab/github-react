e2627e35d2cc4770bfbecbef0ad53f49cdf6313e
Alvaro Oliveira <alvaro.oliveira@compasso.com.br>
2021-06-29 16:01:30 -0300

    fix tests page Home

--- src/__tests__/pages/Home.spec.tsx
+++ src/__tests__/pages/Home.spec.tsx
@@ -1,34 +1,123 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
+import Context from '../../contexts';
 import Home from '../../pages/Home';
 
+import users from '../json/users.json';
+
 describe('Testing Home page', () => {
-  it('should show a load when user submits survey', async () => {
+  it('must load component successfully', async () => {
+    render(<Home />);
+    expect(screen.getByText('Pesquise por um usuário válido do Github')).toBeInTheDocument();
+  });
+  it('should show a load when user submits survey', () => {
+    const server = setupServer(
+      rest.get('https://api.github.com/search/users', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json(users));
+      }),
+    );
+    server.listen();
     render(<Home />);
     const input = screen.getByRole('textbox');
     userEvent.type(input, 'algab');
     fireEvent.keyDown(input, { key: 'Enter', code: 13 });
     expect(document.querySelector('.MuiCircularProgress-svg')).toBeInTheDocument();
+    server.close();
   });
-  it('should show the list of users from the typed name', async () => {
+  it('should show the list of users with the typed name when pressing the enter key', async () => {
+    const server = setupServer(
+      rest.get('https://api.github.com/search/users', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json(users));
+      }),
+    );
+    server.listen();
     render(<Home />);
     const input = screen.getByRole('textbox');
     userEvent.type(input, 'algab');
     fireEvent.keyDown(input, { key: 'Enter', code: 13 });
     expect(document.querySelector('.MuiCircularProgress-svg')).toBeInTheDocument();
     await waitFor(() => expect(screen.getByText('algab')).toBeInTheDocument());
-    expect(screen.getAllByAltText('Profile Photo').length).toBe(8);
+    expect(screen.getAllByAltText('Profile Photo').length).toBe(1);
+    server.close();
   });
-  it('should show a message stating that it cannot find the desired user', async () => {
+  it('should show the list of users with the typed name when pressing the button', async () => {
+    const server = setupServer(
+      rest.get('https://api.github.com/search/users', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json(users));
+      }),
+    );
+    server.listen();
     render(<Home />);
     const input = screen.getByRole('textbox');
-    userEvent.type(input, 'asasaszazasas');
+    userEvent.type(input, 'algab');
+    userEvent.click(screen.getByRole('button', { hidden: true }));
+    expect(document.querySelector('.MuiCircularProgress-svg')).toBeInTheDocument();
+    await waitFor(() => expect(screen.getByText('algab')).toBeInTheDocument());
+    expect(screen.getAllByAltText('Profile Photo').length).toBe(1);
+    server.close();
+  });
+  it('should show a message stating that it cannot find the desired user', async () => {
+    const server = setupServer(
+      rest.get('https://api.github.com/search/users', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json({ items: [] }));
+      }),
+    );
+    server.listen();
+    render(<Home />);
+    const input = screen.getByRole('textbox');
+    userEvent.type(input, 'nick test');
     fireEvent.keyDown(input, { key: 'Enter', code: 13 });
     expect(document.querySelector('.MuiCircularProgress-svg')).toBeInTheDocument();
     await waitFor(() => {
       expect(screen.getByText('Pesquise por um usuário válido do Github')).toBeInTheDocument();
     });
+    server.close();
+  });
+  it('should show a message stating that there was an error', async () => {
+    const server = setupServer(
+      rest.get('https://api.github.com/search/users', (req, res, ctx) => res(ctx.status(500))),
+    );
+    server.listen();
+    render(
+      <Context>
+        <Home />
+      </Context>,
+    );
+    const input = screen.getByRole('textbox');
+    userEvent.type(input, 'nick test');
+    fireEvent.keyDown(input, { key: 'Enter', code: 13 });
+    expect(document.querySelector('.MuiCircularProgress-svg')).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByText('Pesquise por um usuário válido do Github')).toBeInTheDocument();
+    });
+    server.close();
+  });
+  it('should show user lists and select the desired user', async () => {
+    const server = setupServer(
+      rest.get('https://api.github.com/search/users', (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json(users));
+      }),
+    );
+    server.listen();
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <Home />
+      </Router>,
+    );
+    const input = screen.getByRole('textbox');
+    userEvent.type(input, 'algab');
+    fireEvent.keyDown(input, { key: 'Enter', code: 13 });
+    expect(document.querySelector('.MuiCircularProgress-svg')).toBeInTheDocument();
+    await waitFor(() => expect(screen.getByText('algab')).toBeInTheDocument());
+    expect(screen.getAllByAltText('Profile Photo').length).toBe(1);
+    userEvent.click(screen.getByText('Visualizar'));
+    server.close();
   });
 });
